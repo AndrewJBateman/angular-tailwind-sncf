dc0e0999b456a02cb5021f44e367f6c83679880a
AndrewJBateman <gomezbateman@yahoo.com>
2022-06-18 19:19:28 +0200

    fix, tailwind config updated, linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './shared/nav/nav.component';
-import { NotFoundComponent } from './modules/common/not-found/not-found.component';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslocoRootModule } from './transloco-root.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./shared/nav/nav.component";
+import { NotFoundComponent } from "./modules/common/not-found/not-found.component";
+import { HttpClientModule } from "@angular/common/http";
+import { TranslocoRootModule } from "./transloco-root.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    NotFoundComponent
-  ],
+  declarations: [AppComponent, NavComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     TranslocoRootModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ["./src/**/*.{html,ts}"],
-  },
+  content: ["./src/**/*.{html,ts}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
