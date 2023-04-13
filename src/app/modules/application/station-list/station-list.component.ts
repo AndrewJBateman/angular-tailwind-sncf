40c88f208222c71f1010f63be453da39b23483f7
AndrewJBateman <gomezbateman@yahoo.com>
2023-04-13 10:35:41 +0200

    refactor: update form control input method

--- src/app/modules/application/station-list/station-list.component.ts
+++ src/app/modules/application/station-list/station-list.component.ts
@@ -10,29 +10,29 @@ import { StationService } from "./services/station.service";
   templateUrl: "./station-list.component.html",
 })
 export class StationListComponent implements OnInit {
-  @ViewChild("form") form: NgForm | undefined;
-  formSubmitted = false;
   name = environment.application.name;
   stations: any;
   latAverage: number = 0;
   lonAverage: number = 0;
   stationCount = 50;
 
-  // search items
-  stationName = "";
   constructor(private stationService: StationService) {}
 
   ngOnInit(): void {}
 
   // search for stations matching user search input
   // observable displayed in template using Angular async pipe
-  onSubmitStationSearch(): void {
+  onSubmitStationSearch(stationSearch: NgForm): void {
     if (this.stations) {
       this.stations = [];
     }
-    if (this.stationName) {
+    let searchName = stationSearch.form.value.stationName
+    if (searchName) {
       this.stationService
-        .apiStationSearch(this.stationName, this.stationCount)
+        .apiStationSearch(
+          searchName,
+          this.stationCount,
+        )
         .pipe(take(this.stationCount), toArray())
         .subscribe(data => (this.stations = data[0].places));
     }
