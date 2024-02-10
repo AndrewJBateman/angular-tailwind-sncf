0decc62c1eb7060fbea5899159bfc75396349802
AndrewJBateman <gomezbateman@yahoo.com>
2024-02-10 17:19:01 +0100

    refactor: check user i/p, rem take(1) as not required, add comments

--- src/app/modules/common/contact/services/github.service.ts
+++ src/app/modules/common/contact/services/github.service.ts
@@ -1,25 +1,40 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { throwError, Observable } from 'rxjs';
-import { catchError, take } from 'rxjs/operators';
+/**
+ * This is a TypeScript class called GithubService.
+ * It is an Angular service that handles HTTP requests to the GitHub API.
+ * The class has a constructor that injects the HttpClient dependency.
+ * It also has a method called getUser, which takes a user parameter of type string and returns an Observable of type User.
+ * The method first checks if the user parameter is valid, and then constructs the URL for the GitHub API request.
+ * It then makes an HTTP GET request to the API using the HttpClient service.
+ * If an error occurs during the request, the method catches the error and throws a new error with a custom error message.
+ * The method returns the response from the API as an Observable.
+ * The class is decorated with the @Injectable decorator, indicating that it can be injected as a dependency.
+ * The decorator also specifies that the service should be provided at the root level.
+ */
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { throwError, Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
 
-import { User } from '../models/user';
-
-const baseUrl = 'https://api.github.com/users/';
+import { User } from "../models/user";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class GithubService {
   constructor(private http: HttpClient) {}
 
   getUser(user: string): Observable<User> {
-    const userSearchUrl = `${baseUrl + user}`;
+    if (!user || user.trim() === "") {
+      throw new Error("Invalid user parameter");
+    }
+    const githubUrl = environment.GITHUB_BASE_URL;
+    const userSearchUrl = `${githubUrl}${user}`;
     return this.http.get<User>(userSearchUrl).pipe(
-      take(1),
-      catchError((err) => {
-        return throwError(() => err);
-      })
+      catchError(err => {
+        const errorMessage = `Error occurred while fetching user: ${err.message}`;
+        return throwError(() => errorMessage);
+      }),
     );
   }
 }
